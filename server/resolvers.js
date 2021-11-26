00bb367357f99d0bad02ec1409f81d934f838e26
Peter Popluhár <peter.popluhar@heureka.cz>
2021-11-26 15:22:06 +0100

    resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,11 @@
-const Query = {
-    greeting: () => 'Hello world'
-}
+const db = require("./db");
 
-module.exports = {Query}
+const Query = {
+  jobs: () => db.jobs.list(),
+};
+
+const Job = {
+  company: (job) => db.companies.get(job.companyId),
+};
+
+module.exports = { Query, Job };
